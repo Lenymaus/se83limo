07108255653865426f9f3ac811d109bce2c4f0a5
Kuhn, Leonard <leo.kuhn@fau.de>
2021-11-12 10:23:34 +0100

    test function

--- se83limo/function.py
+++ se83limo/function.py
@@ -13,10 +13,12 @@ def imshow(X, resize=None):
     except: print("Oops!  That was no valid array!")
     if im is not None:
         if isinstance(resize, tuple) and len(resize) == 2:
+            display(im.resize(resize))
+            #im.resize(resize).show()
             return im.resize(resize)
-            display(im)
         else: 
             display(im)
+            #im.show()
             return im
     return im
     
