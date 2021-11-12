7e3c88539e4d42a4479154ec36adeafd1a0b7a86
Kuhn, Leonard <leo.kuhn@fau.de>
2021-11-12 09:24:47 +0100

    setup for pip

---
+++ setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+from setuptools import find_packages
+
+
+setup(name='se83limo',
+        version='0.1',
+        author='Leonard Kuhn',
+        author_email='leo.kuhn@fau.de',
+        packages=find_packages(),
+        install_requires=['numpy', 'Pillow', 'ipywidgets'])
