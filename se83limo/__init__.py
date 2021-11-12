af13d7321867b640444de04151d21c1891221373
Kuhn, Leonard <leo.kuhn@fau.de>
2021-11-12 10:15:43 +0100

    test function

--- se83limo/__init__.py
+++ se83limo/__init__.py
@@ -1,2 +1,2 @@
 from .function import *
-__version__ = "0.11"
+__version__ = "0.13"
--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import find_packages
 
 
 setup(name='se83limo',
-        version='0.12',
+        version='0.13',
         author='Leonard Kuhn',
         author_email='leo.kuhn@fau.de',
         packages=find_packages(),
